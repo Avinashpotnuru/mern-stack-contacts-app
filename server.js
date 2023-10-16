39cc253c991ac018df714902fb7dfbb414eb380c
AvinashPotnuru1 <avinashpotnuru@techarion.com>
2023-10-16 15:05:17 +0530

    change local

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 mongoose
-  .connect("mongodb://localhost:27017/mydatabase", {
+  .connect("mongodb://127.0.0.1:27017/mydatabase", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
